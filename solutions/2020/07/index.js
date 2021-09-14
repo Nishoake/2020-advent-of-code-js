e08fa353a3f8a16112d6f1e67558c41a4d40ea7c
Nishoake <nsribava@uwaterloo.ca>
2021-09-13 23:42:51 -0400

    Created a Graph using an adjacency list and traversed using BFS to complete Part 1 for Day 07: Handy Haversacks 💼🛍

--- solutions/2020/07/index.js
+++ solutions/2020/07/index.js
@@ -1,7 +1,7 @@
 const Input = require('./input')
 
-// // Parsing the input as an Array of numbers
-// const getInput = Input.numbers.split('\n').map(Number)
+// Parsing the input as an Array of numbers
+const getInput = Input.rules.split('\n').map(String)
 
 // Create a Graph of the example input, represent as an adjacency matrix
 // Use this to test your algo before implementing the parsing functionality
@@ -18,9 +18,52 @@ const graph = {
   'dotted black': [{ color: 'dark olive', capacity: 4 }, { color: 'vibrant plum', capacity: 6}]
 }
 
+// Notes:
+// 'no other bags.'
+
 // Defined function for part1 of the problem
 const part1 = (input) => {
   
+  // Represent the graph as an adjacency matrix
+  let graph = {}
+
+  // Loop through all the rules
+  for (let i = 0; i < input.length; i++) {
+    // Parse the Housing Color
+    const parsed = input[i].match(/(\w*\s\w*) bags contain (.*)/)
+
+    // console.log(`The ${i} rule = ${parsed[2]}`)
+
+    let housingColor = parsed[1]
+
+    if (!graph[housingColor]) graph[housingColor] = []
+
+    let subColor = parsed[2].split(",")
+
+    if (subColor[0] === 'no other bags.')  continue
+
+    for (let j = 0; j < subColor.length; j++){
+      const deepParsed = subColor[j].match(/(\d) (\w*\s\w*) bag/)
+
+      const Capacity = deepParsed[1]
+      const Color = deepParsed[2]
+
+      // console.log(`The color ${Color} has a capacity of ${Capacity}`)
+
+      if (!graph[Color]) graph[Color] = []
+
+      const package = {
+        color: housingColor,
+        capacity: Capacity
+      }
+
+      graph[Color].push(package)
+    }
+    // console.log(`The subcolors = ${subColor[0]}`)
+  }
+
+  console.log(`The graph = ${JSON.stringify(graph)}`)
+
   let queue = []
   let visited = {}
 
@@ -30,14 +73,15 @@ const part1 = (input) => {
     let currentColor = queue.shift()
     console.log(`currentColor = ${currentColor}`)
 
-    console.log(`Previous Graph = ${JSON.stringify(visited)}`)
+    // console.log(`Previous Graph = ${JSON.stringify(visited)}`)
     if(visited[currentColor]) continue
 
     visited[currentColor] = true
-    console.log(`After Graph = ${JSON.stringify(visited)}`)
+    // console.log(`After Graph = ${JSON.stringify(visited)}`)
 
-    for(let housingColor of input[currentColor]){
-      console.log(`${currentColor}'s associated colors = ${JSON.stringify(housingColor)}`)
+    for(let housingColor of graph[currentColor]){
+      // console.log(`${currentColor}'s associated colors = ${JSON.stringify(housingColor)}`)
+      // console.log(`---`)
 
       queue.push(housingColor.color)
     }
@@ -49,7 +93,7 @@ const part1 = (input) => {
 }
 
 // Execute function for part1 of the problem
-const result1 = part1(graph)
+const result1 = part1(getInput)
 console.log(`Part 1: The number of bag colors that can eventually contain at least one shiny gold bag is = ${result1}`)
 
 
