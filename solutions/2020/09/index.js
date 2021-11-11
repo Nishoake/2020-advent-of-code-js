ee3689993e025996495a81c2cbace51751b70275
Nishoake <nsribava@uwaterloo.ca>
2021-11-11 10:27:34 -0500

    Completed Part One of Day 9: Encoding Error 🛑

--- solutions/2020/09/index.js
+++ solutions/2020/09/index.js
@@ -1,12 +1,32 @@
 const Input = require('./input')
 
 // Parsing the input as an Array of numbers
-const getInput = Input.sample.split('\n').map(Number)
+const getInput = Input.preamble.split('\n').map(Number)
 
 
 // Defined function for part1 of the problem
 const part1 = (input) => {
-  console.log(`The parsed input = ${input}`)
+  // Initialize the preamble
+  const preamble = new Set()
+  for (let index = 0; index < 25; index++)  preamble.add(input[index])
+
+  // Iterate over the input to find the invalid number
+  for (let i = 0; i < input.length - 25; i++) {
+    const target = input[i+25]
+    let sumFound = false
+
+    for (let j = 0; j < 25; j++) {
+      if (preamble.has(target - input[j + i])) {
+        sumFound = true
+        break
+      }
+    }
+
+    if (!sumFound) return target
+
+    preamble.delete(input[i])
+    preamble.add(target)
+  }
 }
 
 // Execute function for part1 of the problem
